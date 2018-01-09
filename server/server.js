63ff22775d6d3bd85ccf33340c3380ddfd2a051e
Jamie Coe <jamiecoe66@gmail.com>
2018-01-09 12:30:53 +0000

    Basic setup

---
+++ server/server.js
@@ -0,0 +1,5 @@
+const app = require('./app');
+
+app.listen(app.get("port"), () => {
+  console.log(`Find the server at: http://localhost:${app.get("port")}/`); // eslint-disable-line no-console
+});
