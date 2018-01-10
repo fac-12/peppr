3669f0f1207cb774f3a1ce546b2817cae724c5c7
Jemila <jemila.abulhawa@gmail.com>
2018-01-10 16:25:51 +0000

    complete jamieoliver url scraping

--- server/controllers/url-scraper/urlScraper.js
+++ server/controllers/url-scraper/urlScraper.js
@@ -1,11 +1,12 @@
 const bbcgf = require('./bbcgf');
+const jamieoliver = require('./jamieoliver');
 
 const urlScraper = (req, res) => {
 
   const { url } = req.body;
 
   if(url.includes('bbcgoodfood')) bbcgf(url, req, res);
-
+  if(url.includes('jamieoliver')) jamieoliver(url, req, res);
 };
 
 module.exports = urlScraper;
