653a065f019990074d7dbc04c8acb2e44d15acc4
Jemila <jemila.abulhawa@gmail.com>
2018-01-11 17:16:39 +0000

    work on landing css

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 import SignUp from './Signup';
 import SignIn from './Signin';
+import landingImage from '../katherine-lenhart-440548.jpg'
 
 class Landing extends Component {
   render() {
     return (
-      <div>
+      <div className="landing">
         {/* <SignUp /> */}
-        <SignIn />
+        <div className="form">
+          <SignIn />
+        </div>
+        <img src={landingImage} alt="Pepper image" className="landingImage" />
       </div>
     );
   }
