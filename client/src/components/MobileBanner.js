5ec768714a21ac902c4e3dbdea28bb49580a50cc
Jamie Coe <jamiecoe66@gmail.com>
2018-01-18 11:13:25 +0000

    Fixed centered grid with left aligned, bit hacky :)

--- client/src/components/MobileBanner.js
+++ client/src/components/MobileBanner.js
@@ -4,8 +4,7 @@ import history from '../actions/history';
 class Banner extends Component {
   render() {
     return(
-    <div className='mobileBanner'>
-      {/* <button onClick={history.goBack} className="mobileBanner__backiconContainer"><i className="ion-ios-arrow-back mobileBanner__backicon"></i></button> */}
+    <div className='mobileBanner'>      
       <h2 className='mobileBanner__title'>{this.props.title}</h2>
     </div>
   )}
--- client/src/components/recipes/RecipeList.js
+++ client/src/components/recipes/RecipeList.js
@@ -5,20 +5,12 @@ import _ from 'lodash';
 import Navbar from '../Navbar';
 
 class RecipeList extends Component {
-  render() {
-    return (
-      <div>
-        <h1>My Recipes</h1>
-        {this.renderPosts()}
-      </div>
-    );
-  }
 
   renderPosts() {
     return _.map(this.props.recipes, recipe => {
       return (
         <Link to={`/recipes/${recipe.id}`} className='recipeList__link recipelist--overlay' key={recipe.id}>
-          <div className='recipeList__link--container' style={{backgroundImage: `linear-gradient(rgba(255,255,255,0.1), rgba(255,255,255,0.1)), url(${recipe.imageurl})`}}>              
+          <div className='recipeList__link--container' style={{backgroundImage: `linear-gradient(rgba(255,255,255,0.1), rgba(255,255,255,0.1)), url(${recipe.imageurl})`}}>
             <h2 className='recipeList__recipeTitle'>{recipe.title}</h2>
           </div>
         </Link>
@@ -26,13 +18,30 @@ class RecipeList extends Component {
     });
   }
 
+  extraPosts() {
+    const remainder = _.size(this.props.recipes) % 3;
+
+    if(remainder) {
+      const remainderArray = [];
+      let i = 0;
+      while(i < (3 - remainder)) {
+        remainderArray.push(<div className="recipeList__link" key={i}></div>)
+        i++;
+      }
+      return remainderArray;
+    }
+  }
+
   render() {
     return (
       <div className='recipeList__container'>
-        <h1 className='recipeList__title'>My Recipes</h1>
-          <div className='recipeList__allrecipes'>
-              {this.renderPosts()}
-          </div>
+        <div className='recipeList__allrecipes'>
+          <div className="recipeList__link"><h1 className='recipeList__title'>My Recipes</h1></div>
+          <div className="recipeList__link"></div>
+          <div className="recipeList__link recipeList__hidden-2x2"></div>
+          {this.renderPosts()}
+          {this.extraPosts()}
+        </div>
         <Navbar />
       </div>
     );
